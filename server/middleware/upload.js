52ed145fc9bdb649372279e570b165daeacc7113
Chakri Pasupuleti <chakri.pasupuleti11@gmail.com>
2026-03-04 23:40:45 +0530

    updated the changes

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -4,15 +4,18 @@ import cloudinary from "../config/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
+  params: async (req, file) => ({
     folder: "task_evidence",
+    resource_type: "auto",            
+    use_filename: true,
+    unique_filename: true,
     allowed_formats: ["jpg", "png", "pdf", "doc", "docx", "jpeg"],
-  },
+  }),
 });
 
 const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, 
+  limits: { fileSize: 10 * 1024 * 1024 },
 });
 
 export default upload;
